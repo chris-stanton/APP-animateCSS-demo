35dddd304d2b477ff7a0e2d2cf1e20e5aebf7f7e
Chris Stanton <cstanton0760@yahoo.com>
2017-06-10 01:27:40 -0500

    first commit

---
+++ scripts/client.js
@@ -0,0 +1,320 @@
+
+var app = angular.module('AngularApp', []);
+
+app.controller('MainController', function () {
+
+    var self = this;
+    self.message = "Angular is sourced correctly"
+
+
+    self.animatecss = [
+      {
+        name : "Flash",
+        animation : "flash"
+      },
+      {
+        name : "Pulse",
+        animation : "pulse"
+      },
+      {
+        name : "Rubber Band",
+        animation : "rubberBand"
+      },
+      {
+        name : "Shake",
+        animation : "shake"
+      },
+      {
+        name : "Head Shake",
+        animation : "headShake"
+      },
+      {
+        name : "Swing",
+        animation : "swing"
+      },
+      {
+        name : "Tada",
+        animation : "tada"
+      },
+      {
+        name : "Wobble",
+        animation : "wobble"
+      },
+      {
+        name : "Jello",
+        animation : "jello"
+      },
+      {
+        name : "Bounce",
+        animation : "bounce"
+      },
+      {
+        name : "Bounce In",
+        animation : "bounceIn"
+      },
+      {
+        name : "Bounce In Down",
+        animation : "bounceInDown"
+      },
+      {
+        name : "Bounce In Left",
+        animation : "bounceInLeft"
+      },
+      {
+        name : "Bounce In Right",
+        animation : "bounceInRight"
+      },
+      {
+        name : "Bounce In Up",
+        animation : "bounceInUp"
+      },
+      {
+        name : "Bounce Out",
+        animation : "bounceOut"
+      },
+      {
+        name : "Bounce Out Down",
+        animation : "bounceOutDown"
+      },
+      {
+        name : "Bounce Out Left",
+        animation : "bounceOutLeft"
+      },
+      {
+        name : "Bounce Out Right",
+        animation : "bounceOutRight"
+      },
+      {
+        name : "Bounce Out Up",
+        animation : "bounceOutUp"
+      },
+      {
+        name : "Fade In",
+        animation : "fadeIn"
+      },
+      {
+        name : "Fade In Down",
+        animation : "fadeInDown"
+      },
+      {
+        name : "Fade In Down Big",
+        animation : "fadeInDownBig"
+      },
+      {
+        name : "Fade In Left",
+        animation : "fadeInLeft"
+      },
+      {
+        name : "Fade In Left Big",
+        animation : "fadeInLeftBig"
+      },
+      {
+        name : "Fade In Right",
+        animation : "fadeInRight"
+      },
+      {
+        name : "Fade In Right Big",
+        animation : "fadeInRightBig"
+      },
+      {
+        name : "Fade In Up",
+        animation : "fadeInUp"
+      },
+      {
+        name : "Fade In Up Big",
+        animation : "fadeInUpBig"
+      },
+      {
+        name : "Fade Out",
+        animation : "fadeOut"
+      },
+      {
+        name : "Fade Out Down",
+        animation : "fadeOutDown"
+      },
+      {
+        name : "Fade Out Down Big",
+        animation : "fadeOutDownBig"
+      },
+      {
+        name : "Fade Out Left",
+        animation : "fadeOutLeft"
+      },
+      {
+        name : "Fade Out Left Big",
+        animation : "fadeOutLeftBig"
+      },
+      {
+        name : "Fade out Right",
+        animation : "fadeOutRight"
+      },
+      {
+        name : "Fade Out Right Big",
+        animation : "fadeOutRightBig"
+      },
+      {
+        name : "Fade out Up",
+        animation : "fadeOutUp"
+      },
+      {
+        name : "Fade Out Big Up",
+        animation : "fadeOutUpBig"
+      },
+      {
+        name : "Flip In-X",
+        animation : "flipInX"
+      },
+      {
+        name : "Flip In-Y",
+        animation : "flipInY"
+      },
+      {
+        name : "Flip Out-X",
+        animation : "flipOutX"
+      },
+      {
+        name : "Flip Out-Y",
+        animation : "flipOutY"
+      },
+      {
+        name : "Light Speed In",
+        animation : "lightSpeedIn"
+      },
+      {
+        name : "Light Speed Out",
+        animation : "lightSpeedOut"
+      },
+      {
+        name : "Rotate In",
+        animation : "rotateIn"
+      },
+      {
+        name : "Rotate In Down Left",
+        animation : "rotateInDownLeft"
+      },
+      {
+        name : "Rotate In Down Right",
+        animation : "rotateInDownRight"
+      },
+      {
+        name : "Rotate In Up Left",
+        animation : "rotateInUpLeft"
+      },
+      {
+        name : "Rotate In Up Right",
+        animation : "rotateInUpRight"
+      },
+      {
+        name : "Rotate Out",
+        animation : "rotateOut"
+      },
+      {
+        name : "Rotate Out Down Left",
+        animation : "rotateOutDownLeft"
+      },
+      {
+        name : "Rotate Out Down Right",
+        animation : "rotateOutDownRight"
+      },
+      {
+        name : "Rotate Out Up Left",
+        animation : "rotateOutUpLeft"
+      },
+      {
+        name : "Rotate Out Up Right",
+        animation : "rotateOutUpRight"
+      },
+      {
+        name : "Hinge",
+        animation : "hinge"
+      },
+      {
+        name : "Jack In The Box",
+        animation : "jackInTheBox"
+      },
+      {
+        name : "Roll In",
+        animation : "rollIn"
+      },
+      {
+        name : "Roll Out",
+        animation : "rollOut"
+      },
+      {
+        name : "Zoom In",
+        animation : "zoomIn"
+      },
+      {
+        name : "Zoom In Down",
+        animation : "zoomInDown"
+      },
+      {
+        name : "Zoom In Left",
+        animation : "zoomInLeft"
+      },
+      {
+        name : "Zoom In Right",
+        animation : "zoomInRight"
+      },
+      {
+        name : "Zoom In Up",
+        animation : "zoomInUp"
+      },
+      {
+        name : "Zoom Out",
+        animation : "zoomOut"
+      },
+      {
+        name : "Zoom Out Down",
+        animation : "zoomOutDown"
+      },
+      {
+        name : "Zoom Out Left",
+        animation : "zoomOutLeft"
+      },
+      {
+        name : "Zoom Out Right",
+        animation : "zoomOutRight"
+      },
+      {
+        name : "Zoom Out Up",
+        animation : "zoomOutUp"
+      },
+      {
+        name : "Slide In Down",
+        animation : "slideInDown"
+      },
+      {
+        name : "Slide In Left",
+        animation : "slideInLeft"
+      },
+      {
+        name : "Slide In Right",
+        animation : "slideInRight"
+      },
+      {
+        name : "Slide In Up",
+        animation : "slideInUp"
+      },
+      {
+        name : "Slide Out Down",
+        animation : "slideOutDown"
+      },
+      {
+        name : "Slide Out Left",
+        animation : "slideOutLeft"
+      },
+      {
+        name : "Slide Out Right",
+        animation : "slideOutRight"
+      },
+      {
+        name : "Slide Out Up",
+        animation : "slideOutUp"
+      }
+
+  ];
+
+
+
+});//ennd of app.controller
